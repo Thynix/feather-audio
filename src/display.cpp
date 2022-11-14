011f793880e0bff8c0d3d49090b9ca1b03d410e3
Steve Dougherty <steve@asksteved.com>
2022-11-14 04:25:47 -0500

    Avoid unnecessary wrapping; hold at end when all overflow displayed

--- src/display.cpp
+++ src/display.cpp
@@ -14,8 +14,10 @@ const int previous_text_str_len = 255;
 class ScrollArea {
 private:
   int16_t starting_y;
-  int height;
+  int16_t height;
   int scroll_frame_interval;
+  // How much lower is a subsequent line below the previous?
+  uint8_t y_advance;
 
   char previous_text[previous_text_str_len + 1] = {};
   bool needs_scrolling;
@@ -24,14 +26,18 @@ private:
   uint16_t font_height;
   const GFXfont *font;
   int font_scale;
+  uint16_t line_count;
 public:
   // Scroll speed - frames to wait between scroll movements. See target_frametime.
-  ScrollArea(int16_t starting_y, int height, const GFXfont *font, int font_scale, int scroll_frame_interval) {
+  ScrollArea(int16_t starting_y, uint16_t height, const GFXfont *font, int font_scale, int scroll_frame_interval) {
     this->starting_y = starting_y;
     this->height = height;
     this->font = font;
     this->font_scale = font_scale;
     this->scroll_frame_interval = scroll_frame_interval;
+    // Default built-in font is NULL and 6x8.
+    this->y_advance = font ? font->yAdvance : font_scale * 8;
+    this->line_count = max(height / y_advance, 1);
   }
 
   // Display text, and scroll if necessary on repeated calls.
@@ -40,8 +46,7 @@ public:
     bool text_changed = strcmp(previous_text, text);
     if (!text_changed && !needs_scrolling)
       return false;
-    
-    display.setTextWrap(false);
+
     display.setFont(font);
     display.setTextSize(font_scale);
 
@@ -56,14 +61,17 @@ public:
       }
 
       int16_t x1, y1;
+      uint16_t wrap_width, wrap_height;
+
+      // Dimensions with wrap.
+      display.setTextWrap(true);
+      display.getTextBounds(text, 0, 0, &x1, &y1, &wrap_width, &wrap_height);
+
+      // Dimensions without wrap
+      display.setTextWrap(false);
       display.getTextBounds(text, 0, 0, &x1, &y1, &total_text_width, &font_height);
 
-      int lines = height / font_height;
-
-      needs_scrolling = total_text_width > 0.7*(display_width * lines);
-
-      //Serial.printf("Displaying '%s' - font height %hu, %d lines, scrolling %d\r\n", text, font_height, lines, needs_scrolling);
-      //Serial.printf("Text changed to '%s'\r\n", text);
+      needs_scrolling = wrap_height > height;
     } else {
       // No text change, but may need to scroll if the frame advanced past the interval.
       scroll_frame++;
@@ -71,47 +79,58 @@ public:
         return false;
     }
 
+    // Enable text wrap only when not scrolling. Done after scrolling determination so it's correct on the first update.
+    display.setTextWrap(!needs_scrolling);
+
     // Before updating, clear previously displayed text within the area.
     display.fillRect(0, starting_y, display_width, height, SH110X_BLACK);
-    //display.clearDisplay();
 
     int offset = scroll_frame / scroll_frame_interval;
 
     // Hold for this many frame intervals at the start/end of the string instead of immediately continuing.
     const int ends_hold = 14;
+    // How much offset is needed for the end of the text to be displayed
+    uint16_t text_overflow = (total_text_width % display_width) + 1;
     if (offset <= ends_hold) {
       // Hold at start
       offset = 0;
-    } else if (offset <= total_text_width) {
+    } else if (offset <= text_overflow + ends_hold) {
       // Compensate for holding at start
       offset -= ends_hold;
-    } else if (offset > total_text_width + ends_hold) {
-      // Hold at end
-      offset = total_text_width;
     } else {
-      // Entire line has been displayed. Reset scrolling.
-      // TODO: this may need to consider how much the display can fit.
-      scroll_frame = 0;
+      // Reset scrolling after end hold
+      if (offset > text_overflow + 2*ends_hold ) {
+        scroll_frame = 0;
+      }
+
+      // Hold at end
+      offset = text_overflow;
     }
 
-    for (int i = 0; i < height / font_height; i++) {
+    for (int i = 0; i < line_count; i++) {
       int x = -offset - i*display_width;
       int custom_font_offset = font != NULL ? font_height : 0;
-      int y = starting_y + custom_font_offset + i*font_height;
+      int y = starting_y + custom_font_offset + i*y_advance;
 
       display.setCursor(x, y);
       display.print(text);
+
+      // Write only the first frame if not scrolling because only the first
+      // line needs to be written when not scrolling because it'll just wrap.
+      if (!needs_scrolling)
+        break;
     }
 
     return true;
   }
 };
 
-// FreeSansBold9pt7b has a font height of 13.
-// Default font at 2x has one of 16.
+// Which font to use. Change to NULL for default 6x8 font.
+const GFXfont *font = &FreeSansBold9pt7b;
+const uint8_t text_size = 1;
 
-ScrollArea topLines(0, 48, &FreeSansBold9pt7b, 1, 1);
-ScrollArea bottomLine(48, 16, &FreeSansBold9pt7b, 1, 1);
+ScrollArea topLines(0, 48, font, 1, 1);
+ScrollArea bottomLine(48, 16, font, 1, 1);
 
 bool display_setup()
 {
@@ -125,7 +144,7 @@ bool display_setup()
   }
 
   display.setRotation(1);
-  display.setFont(&FreeSansBold9pt7b);
+
   display.setTextColor(SH110X_WHITE);
 
   return true;
@@ -144,6 +163,6 @@ bool display_text(const char* top, const char* bottom)
 
   if (display_changed)
     display.display();
-  
+
   return display_changed;
 }
